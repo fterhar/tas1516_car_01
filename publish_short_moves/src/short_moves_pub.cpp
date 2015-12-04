bf05bc7295c58b577b28ca6cd645c101842cb1cc
Anthony <a.carnevale@tum.de>
2015-12-04 10:52:46 +0100

    [chg]	publish_short_moves optimiert

--- publish_short_moves/src/short_moves_pub.cpp
+++ publish_short_moves/src/short_moves_pub.cpp
@@ -8,35 +8,47 @@ int main(int argc, char **argv)
 
   control move_control;
 
-  ros::Rate loop_rate(4); //every 250 ms (4 Hz)
+  ros::Rate loop_rate(50); //every 250 ms (4 Hz)
 
   int count = 0;
   while (ros::ok())	//this returns false if e.g. CTRL-C is hit
   {
     
 
-    if (count == 0)	//first accelerate
+    if (count >= 0 && count < 50)	//first accelerate
     {
 	move_control.control_servo.x = 1550;
 	move_control.control_servo_pub_.publish(move_control.control_servo);	//broadcast message to anyone who is connected
     }
     
-    else if (count == 1)	//then break
+    else if (count >= 50 && count < 100)	//then break
     {
 	move_control.control_servo.x = 1500;
 	move_control.control_servo_pub_.publish(move_control.control_servo);	//broadcast message to anyone who is connected
     }
 
-    else if (count == 2)	//after that move backwards...
+    else if (count >= 100 && count < 150)	//after that move backwards...
     {
 	move_control.control_servo.x = 1300;
 	move_control.control_servo_pub_.publish(move_control.control_servo);	//broadcast message to anyone who is connected
     }
     
-    else if (count == 3)	//and break
+    else if (count >= 150 && count < 200)	//and break
     {
 	move_control.control_servo.x = 1500;
 	move_control.control_servo_pub_.publish(move_control.control_servo);	//broadcast message to anyone who is connected
+    }
+
+    else if (count >= 200 && count < 250)	//move again backwards (two times backwards needed after having driven foreward, otherwise it does not work!)
+    {
+    move_control.control_servo.x = 1300;
+    move_control.control_servo_pub_.publish(move_control.control_servo);	//broadcast message to anyone who is connected
+    }
+
+    else if (count >= 250 && count < 300)	//and break
+    {
+    move_control.control_servo.x = 1500;
+    move_control.control_servo_pub_.publish(move_control.control_servo);	//broadcast message to anyone who is connected
     }
 
     ros::spinOnce();	//provide callbacks
