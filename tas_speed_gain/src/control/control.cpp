c53534c5115a8572fa6805a8a22022a923337420
Fynn Terhar <fynn.terhar@tum.de>
2015-12-05 01:33:43 +0100

    [chg] Changed control.cpp
    Should now detect when robot enters boost area.
    For unknown reason it does not work yet.
    
    To be fixed.

--- tas_speed_gain/src/control/control.cpp
+++ tas_speed_gain/src/control/control.cpp
@@ -14,7 +14,8 @@ control::control()
      R1 = Rect(Point(25.1, 8.65), Point(21.3, 17.1));
      R2 = Rect(Point(20.3, 17.5), Point(12.5, 20.4));
      R3 = Rect(Point(10.3, 18.3), Point(11.2, 9.46));
-     R4 = Rect(Point(12.5, 3.71), Point(20.3, 9.15));
+    // R4 = Rect(Point(12.5, 3.71), Point(20.3, 9.15));
+     R4 = Rect(Point(1.5, 1.71), Point(200.3, 200.15));
 
 }
 
@@ -29,44 +30,15 @@ void control::cmdCallback(const geometry_msgs::Twist::ConstPtr& msg)
 
 void control::poseCallback(const geometry_msgs::PoseStamped::ConstPtr& pose)
 {
-   // if (pose)
+    if (pose){
+	Point p(pose.get()->pose.position.x, pose.get()->pose.position.y);
+	if(R1.isInside(p) || R2.isInside(p) || R3.isInside(p) || R4.isInside(p)){
+	    ROS_INFO("[INFO] Is in Boost area.");
+        } else {
+	    ROS_INFO("[INFO] Is not in Boost area.");
+	}
+    } 
 	//pose_msg = *pose;
     
 }
 
-// a flag method that tells us if we are controlling the car manually or automatically
-//void control::wiiCommunicationCallback(const std_msgs::Int16MultiArray::ConstPtr& msg)
-//{
-//    control_Mode.data = msg->data[0];
-//    control_Brake.data = msg->data[1];
-//}
-
-//geometry_msgs::Vector3 control::P_Controller()
-//{
-//    current_ServoMsg.x = previous_ServoMsg.x + Fp*(cmd_linearVelocity - odom_linearVelocity);
-
-//    current_ServoMsg.y = cmd_steeringAngle;
-
-
-//    if(current_ServoMsg.x > 1580)
-//    {
-//        current_ServoMsg.x = 1580;
-//    }
-//    else if(current_ServoMsg.x < 1300)
-//    {
-//        current_ServoMsg.x = 1300;
-//    }
-
-//    if(current_ServoMsg.y > 2000)
-//    {
-//        current_ServoMsg.y = 2000;
-//    }
-//    else if(current_ServoMsg.y < 1000)
-//    {
-//        current_ServoMsg.y = 1000;
-//    }
-
-//    previous_ServoMsg = current_ServoMsg;
-
-//    return current_ServoMsg;
-//}
