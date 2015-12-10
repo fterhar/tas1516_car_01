c3f8be15add64916527e1b993d9d8eb8e5979537
Anthony <a.carnevale@tum.de>
2015-12-10 21:29:19 +0100

    [chg] modified cmakelists

--- velocity_control/src/control_vel.cpp
+++ velocity_control/src/control_vel.cpp
@@ -20,11 +20,11 @@ void realCallback(const nav_msgs::Odometry::ConstPtr& odom)
   ROS_INFO("Current velocity: [%lf,%lf,%lf]", linear_vel_x, linear_vel_y, angular_vel); //store x,y,z position values
 }
 
-void neededCallback(const /* typ cmd_vel */)
+/*void neededCallback(const  typ cmd_vel )
 {
   //cmd_vel speichern
 }
-
+*/
 int main(int argc, char **argv)
 {
   
@@ -34,7 +34,7 @@ int main(int argc, char **argv)
 
   
   ros::Subscriber real_vel = n.subscribe("tas_odom", 1000, realCallback);
-  ros::Subscriber needed_vel = n.subscribe("cmd_vel", 1000, neededCallback);
+  //ros::Subscriber needed_vel = n.subscribe("cmd_vel", 1000, neededCallback);
   ros::Publisher ctrl_vel_pub = nh.advertise<nav_msgs::Odometry>("tas_cmd_vel", 1000);
   
 
